dbdb799cfd39b6e5fca116e257a9de32f1160576
Prashant Gupta <antshpra@gmail.com>
2024-02-07 12:16:35 +0530

    implementation

--- src/index.js
+++ src/index.js
@@ -1,40 +1,21 @@
-const PROJECT_ID = {
-  alpha:   "synergybyte-test",
-  beta:    "synergybyte-test",
-  gamma:   "synergybyte-gamma",
-  offline: "synergybyte-offline",
-  prod:    "synergybyte-prod"
-}[process.env.NODE_ENV];
+const { PubSub } = require('@google-cloud/pubsub');
 
 
 
-let pubsub = new (require('@google-cloud/pubsub').PubSub)();
+exports.init = ({ projectId, topic }) => {
 
-class PubSub {
+  const pubsub = new PubSub({ projectId });
+  topic = pubsub.topic(`projects/${ projectId }/topics/${ topic }`);
 
-  constructor(topic) {
-    this.topic = pubsub.topic(`projects/${ PROJECT_ID }/topics/${ topic }`);
+  exports.send = async (service, api, params) => {
+    await topic.publishJSON(params, {
+      "Content-Type": "application/json",
+      'x-zero65-sender': process.env.npm_package_name,
+      'x-zero65-receiver': service,
+      'x-zero65-receiver-url': api
+    });
   }
 
-  message(req) {
-    return {
-      data: JSON.parse(Buffer.from(req.body.message.data, 'base64').toString('utf-8')),
-      attributes: req.body.message.attributes
-    }
-  }
-
-  publish(req, data, attributes) {
-    if(!attributes && req && req.headers && req.headers['x-cloud-trace-context']) {
-      let traceContext = req.headers['x-cloud-trace-context'].split(/[/;]/);
-      attributes = {
-        'logging.googleapis.com/trace':        `projects/${ PROJECT_ID }/traces/${ traceContext[0] }`,
-        'logging.googleapis.com/spanId':        traceContext[1],
-        'logging.googleapis.com/trace_sampled': traceContext[2] == 'o=1' ? 'true' : 'false'
-      }
-    }
-    return this.topic.publish(Buffer.from(JSON.stringify(data)), attributes);
-  }
+  delete exports.init;
 
 }
-
-module.exports = PubSub;
